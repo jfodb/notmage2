8902c4d2829c04b4c9430d4a3af234b6dea35a97
twilson <thomasscot@gmail.com>
2018-03-22 15:07:44 -0400

    added docker support files

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ EXPOSE 80
 
 CMD ["/magento/docker/serve.sh"]
 
-ENV PATH /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+ENV PATH /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/magento/bin
 
 RUN locale-gen --no-purge en_US.UTF-8
 ENV LC_ALL en_US.UTF-8
@@ -37,12 +37,12 @@ RUN apt-add-repository -y ppa:ondrej/php \
     && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
     && curl -sS https://getcomposer.org/installer | php \
     && mv composer.phar /usr/local/bin/composer \
-		&& export PATH=$PATH:/magento/mage2/bin
-
-
+		&& export PATH=$PATH:/magento/bin
 
 COPY ./docker/nginx.conf /etc/nginx/sites-available/default
 
 COPY ./docker/php.ini /etc/php/7.0/fpm/php.ini
+
+COPY ./docker/env.php /magento/app/env.php
 
 ADD . /magento
--- docker/env.php
+++ docker/env.php
@@ -1,23 +1,44 @@
 <?php
 return array (
-  'backend' => 
+  'backend' =>
   array (
     'frontName' => 'odbmadmin',
   ),
-  'crypt' => 
+  'crypt' =>
   array (
     'key' => '782d87cee4ed0cec98ebb2ffaeee75c1',
   ),
-  'session' => 
-  array (
-    'save' => 'files',
-  ),
-  'db' => 
+	'session' =>
+	array (
+	  'save' => 'redis',
+	  'redis' =>
+	  array (
+	    'host' => 'redis',
+	    'port' => '6379',
+	    'password' => '',
+	    'timeout' => '2.5',
+	    'persistent_identifier' => '',
+	    'database' => '2',
+	    'compression_threshold' => '2048',
+	    'compression_library' => 'gzip',
+	    'log_level' => '1',
+	    'max_concurrency' => '6',
+	    'break_after_frontend' => '5',
+	    'break_after_adminhtml' => '30',
+	    'first_lifetime' => '600',
+	    'bot_first_lifetime' => '60',
+	    'bot_lifetime' => '7200',
+	    'disable_locking' => '0',
+	    'min_lifetime' => '60',
+	    'max_lifetime' => '2592000'
+	  )
+	),
+  'db' =>
   array (
     'table_prefix' => '',
-    'connection' => 
+    'connection' =>
     array (
-      'default' => 
+      'default' =>
       array (
         'host' => 'mysql',
         'dbname' => 'devdhporg',
@@ -30,24 +51,24 @@ return array (
       ),
     ),
   ),
-  'cache' => 
+  'cache' =>
   array (
-    'frontend' => 
+    'frontend' =>
     array (
-      'default' => 
+      'default' =>
       array (
         'backend' => 'Cm_Cache_Backend_Redis',
-        'backend_options' => 
+        'backend_options' =>
         array (
           'server' => 'redis',
           'database' => '0',
           'port' => '6379',
         ),
       ),
-      'page_cache' => 
+      'page_cache' =>
       array (
         'backend' => 'Cm_Cache_Backend_Redis',
-        'backend_options' => 
+        'backend_options' =>
         array (
           'server' => 'redis',
           'port' => '6379',
@@ -57,16 +78,16 @@ return array (
       ),
     ),
   ),
-  'resource' => 
+  'resource' =>
   array (
-    'default_setup' => 
+    'default_setup' =>
     array (
       'connection' => 'default',
     ),
   ),
   'x-frame-options' => 'SAMEORIGIN',
   'MAGE_MODE' => 'developer',
-  'cache_types' => 
+  'cache_types' =>
   array (
     'config' => 1,
     'layout' => 1,
@@ -82,7 +103,7 @@ return array (
     'translate' => 1,
     'config_webservice' => 1,
   ),
-  'install' => 
+  'install' =>
   array (
     'date' => 'Tue, 08 Aug 2017 16:23:37 +0000',
   ),
