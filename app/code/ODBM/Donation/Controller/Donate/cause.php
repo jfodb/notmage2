bf05610380e508fc0c3eec65084efbcacbc97c5f
bjclark <bjclark@Remmert-MAC.local>
2018-05-16 16:08:33 -0400

    remove php_ini settings

--- app/code/ODBM/Donation/Controller/Donate/cause.php
+++ app/code/ODBM/Donation/Controller/Donate/cause.php
@@ -3,10 +3,6 @@ namespace ODBM\Donation\Controller\Donate;
 
 use Magento\Framework\App\Action\Context;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 class Cause extends \Magento\Framework\App\Action\Action
 {
 	protected $_productRepository;
@@ -43,8 +39,16 @@ class Cause extends \Magento\Framework\App\Action\Action
 		// Get sku from configuration
 		try {
 			$sku = $this->_scopeConfig->getValue('odbmdonations/general/default_donate', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+
+			// support for multiple default options, as
+			// comma-separated list
+			if ( !empty( $sku_arr = explode( ',', $sku ) ) ) {
+				var_dump($sku);
+				// Note: not using array_rand()
+				$sku = trim( $sku_arr[mt_rand(0, count( $sku_arr ) - 1)] );
+			}
 		} catch ( \Exception $e ) {
-			echo ($e->getMessage());
+			echo( $e->getMessage() );
 			die('Exception in Cause::get_default_sku()');
 		}
 
@@ -53,10 +57,13 @@ class Cause extends \Magento\Framework\App\Action\Action
 
 	protected function get_product_url_by_motivation( $motivation_code = false ) {
 		$product_url = false;
+		$used_default = false;
 
 		if ( !$motivation_code ) {
 			// Get default motivation code
 			$motivation_code = $this->get_default_sku();
+
+			$used_default = true;
 		}
 
 		if ( $motivation_code ) {
@@ -70,12 +77,20 @@ class Cause extends \Magento\Framework\App\Action\Action
 				} else {
 					// If product doesn't exist, we want to call this
 					// function again to get the default product url
-					$product_url = $this->get_product_url_by_motivation();
+					if ( !$used_default ) {
+						$product_url = $this->get_product_url_by_motivation();
+					} else {
+						throw new \Exception( 'Default sku ({$motivation_code}) is not a product!' );
+					}
 				}
 			} catch( \Exception $e ) {
 				// If product doesn't exist, we want to call this
 				// function again to get the default product url
-				$product_url = $this->get_product_url_by_motivation();
+				if ( !$used_default ) {
+					$product_url = $this->get_product_url_by_motivation();
+				} else {
+					throw new \Exception( "Default sku ({$motivation_code}) is not a product!" );
+				}
 			}
 		}
 
