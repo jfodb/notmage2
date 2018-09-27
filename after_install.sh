f85baae27dd79389cc67861b679e0bed9d6ace2b
bjclark <bjclark@Remmert-MAC.local>
2018-09-27 15:47:38 -0400

    Remove remove appspec sudo, it's already run as root.

--- after_install.sh
+++ after_install.sh
@@ -2,12 +2,13 @@
 if [[ $(findmnt -m /usr/share/nginx/html/magento/pub/media) ]]; then
     echo "Mounted"
 else
-    sudo mount -t efs fs-1e74a656:/ /usr/share/nginx/html/magento/pub/media/
+    mount -t efs fs-1e74a656:/ /usr/share/nginx/html/magento/pub/media/
 fi
-sudo php /usr/share/nginx/html/magento/bin/magento setup:upgrade
-sudo php /usr/share/nginx/html/magento/bin/magento setup:static-content:deploy
-sudo chown -R apache:nginx /usr/share/nginx/html/magento
 
-sudo rm -rf /usr/share/nginx/html/magento/var/cache/* /usr/share/nginx/html/magento/var/page_cache/* /usr/share/nginx/html/magento/var/composer_home/* /usr/share/nginx/html/magento/var/tmp/*
-sudo php /usr/share/nginx/html/magento/bin/magento setup:di:compile
-sudo php /usr/share/nginx/html/magento/bin/magento cache:clean
+php /usr/share/nginx/html/magento/bin/magento setup:upgrade
+php /usr/share/nginx/html/magento/bin/magento setup:static-content:deploy
+chown -R apache:nginx /usr/share/nginx/html/magento
+
+rm -rf /usr/share/nginx/html/magento/var/cache/* /usr/share/nginx/html/magento/var/page_cache/* /usr/share/nginx/html/magento/var/composer_home/* /usr/share/nginx/html/magento/var/tmp/*
+php /usr/share/nginx/html/magento/bin/magento setup:di:compile
+php /usr/share/nginx/html/magento/bin/magento cache:clean
