7aab8a8fabaeb2db3f4edcb6c518d395be10647e
bjclark <bjclark@Remmert-MAC.local>
2018-09-27 15:45:02 -0400

    Make changes to after_install script to compile di

--- after_install.sh
+++ after_install.sh
@@ -2,9 +2,12 @@
 if [[ $(findmnt -m /usr/share/nginx/html/magento/pub/media) ]]; then
     echo "Mounted"
 else
-    mount -t efs fs-1e74a656:/ /usr/share/nginx/html/magento/pub/media/
+    sudo mount -t efs fs-1e74a656:/ /usr/share/nginx/html/magento/pub/media/
 fi
-php /usr/share/nginx/html/magento/bin/magento setup:upgrade
-php /usr/share/nginx/html/magento/bin/magento setup:static-content:deploy
-chown -R apache:nginx /usr/share/nginx/html/magento
-php /usr/share/nginx/html/magento/bin/magento cache:clean
+sudo php /usr/share/nginx/html/magento/bin/magento setup:upgrade
+sudo php /usr/share/nginx/html/magento/bin/magento setup:static-content:deploy
+sudo chown -R apache:nginx /usr/share/nginx/html/magento
+
+sudo rm -rf /usr/share/nginx/html/magento/var/cache/* /usr/share/nginx/html/magento/var/page_cache/* /usr/share/nginx/html/magento/var/composer_home/* /usr/share/nginx/html/magento/var/tmp/*
+sudo php /usr/share/nginx/html/magento/bin/magento setup:di:compile
+sudo php /usr/share/nginx/html/magento/bin/magento cache:clean
