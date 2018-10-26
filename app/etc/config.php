882fa090bca925af303d4f8e4f7ac7e322138e5a
bjclark <bjclark@Remmert-MAC.local>
2018-10-25 23:41:52 -0400

    Style fixes to Rob's code

--- app/etc/config.php
+++ app/etc/config.php
@@ -2,9 +2,11 @@
 return array (
   'modules' => 
   array (
+    'Dat_ShippingAddress' => 1,
     'Magento_Store' => 1,
     'Magento_Directory' => 1,
     'Magento_Theme' => 1,
+    'MagePal_GoogleTagManager' => 1,
     'Magento_Eav' => 1,
     'Magento_AdvancedPricingImportExport' => 1,
     'Magento_Config' => 1,
@@ -123,10 +125,11 @@ return array (
     'Mageplaza_Core' => 1,
     'Mageplaza_Smtp' => 1,
     'ODBM_Donation' => 1,
-    'Psuedo_Mpxdownload' => 1,
     'ODBM_Paperless' => 1,
+    'Psuedo_Mpxdownload' => 1,
     'Psuedo_Silverpop' => 1,
     'Shopial_Facebook' => 1,
     'Temando_Shipping' => 1,
+    'Yireo_GoogleTagManager2' => 1,
   ),
 );
