df6eb2f118b56133438ff24b8c59e0a2d513a414
bjclark <bjclark@Remmert-MAC.local>
2018-12-19 12:01:43 -0500

    Update ymi-logo reference

--- app/design/frontend/Odbm/ymi/registration.php
+++ app/design/frontend/Odbm/ymi/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
 	\Magento\Framework\Component\ComponentRegistrar::THEME,
-	'frontend/Odbm/ymi_child',
+	'frontend/Odbm/ymi',
 	__DIR__
 );
