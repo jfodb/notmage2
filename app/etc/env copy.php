935391ce791b41110283f8d94dbc3ebf89999d15
Mike Dubinsky <mike.dubinsky@odb.org>
2019-02-18 14:19:56 -0500

    Updated 2.3 Files

---
+++ app/etc/env copy.php
@@ -0,0 +1,54 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_1wbqks'
+    ],
+    'crypt' => [
+        'key' => '782d87cee4ed0cec98ebb2ffaeee75c1'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'mysql',
+                'dbname' => 'magento',
+                'username' => 'magento_user',
+                'password' => 'magento',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Mon, 18 Feb 2019 15:20:23 +0000'
+    ]
+];
