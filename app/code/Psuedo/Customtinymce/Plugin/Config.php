9bc1a688adfe1eb7da5496b793ddc017a81417c6
Mike Dubinsky <mike.dubinsky@odb.org>
2019-03-04 11:19:12 -0500

    Only apply configuration changes if wysiwyg editor is Tinymce v4

--- app/code/Psuedo/Customtinymce/Plugin/Config.php
+++ app/code/Psuedo/Customtinymce/Plugin/Config.php
@@ -8,6 +8,14 @@ namespace Psuedo\Customtinymce\Plugin;
 
 class Config
 {
+
+    protected $activeEditor;
+
+    public function __construct(\Magento\Ui\Block\Wysiwyg\ActiveEditor $activeEditor)
+    {
+        $this->activeEditor = $activeEditor;
+    }
+
     /**
      * Return WYSIWYG configuration
      *
@@ -19,31 +27,39 @@ class Config
         \Magento\Ui\Component\Wysiwyg\ConfigInterface $configInterface,
         \Magento\Framework\DataObject $result
     ) {
-        if (($result->getDataByPath('settings/toolbar')) || ($result->getDataByPath('settings/plugins'))|| ($result->getDataByPath('settings/extended_valid_elements')) || ($result->getDataByPath('settings/valid_children')) || ($result->getDataByPath('settings/verify_html'))){
-            // do not override ui_element config
+
+        // Get current wysiwyg adapter's path
+        $editor = $this->activeEditor->getWysiwygAdapterPath();
+
+        // Is the current wysiwyg tinymce v4?
+        if(strpos($editor,'tinymce4Adapter')){
+            if (($result->getDataByPath('settings/toolbar')) || ($result->getDataByPath('settings/plugins'))|| ($result->getDataByPath('settings/extended_valid_elements')) || ($result->getDataByPath('settings/valid_children')) || ($result->getDataByPath('settings/verify_html'))){
+                // do not override ui_element config
+                return $result;
+            }
+
+            $settings = $result->getData('settings');
+
+            if (!is_array($settings)) {
+                $settings = [];
+            }
+
+            // configure tinymce settings
+            // configure toolbar (loaded toolbar with color selector, image inserter, link creator, and code button
+            $settings['toolbar'] = 'undo redo | styleselect | fontsizeselect | forecolor backcolor | bold italic underline strikethrough | numlist bullist | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table | link image | code';
+            $settings['plugins'] = 'textcolor lists image link code';
+            // allow <script> and <style> tags
+            $settings['extended_valid_elements'] = 'script[*],style[*]';
+            $settings['valid_children'] = '+body[script|style]';
+            $settings['valid_elements'] = '*[*]';
+            // to stop wrapping everything in <p> tags (including <scripts>). Also, fixes error if <style> is first element in HTML editor.
+            // $settings['forced_root_block'] ='';
+
+            $result->setData('settings', $settings);
+            return $result;
+    }
+        else{ // don't make any changes if the current wysiwyg editor is not tinymce 4
             return $result;
         }
-
-        $settings = $result->getData('settings');
-
-        if (!is_array($settings)) {
-            $settings = [];
-        }
-
-        // configure tinymce settings
-        // configure toolbar (loaded toolbar with color selector, image inserter, link creator, and code button
-        $settings['toolbar'] = 'undo redo | styleselect | fontsizeselect | forecolor backcolor | bold italic underline strikethrough | numlist bullist | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table | link image | code';
-        $settings['plugins'] = 'textcolor lists image link code';
-        // allow <script> and <style> tags
-        $settings['extended_valid_elements'] = 'script[*],style[*]';
-        $settings['valid_children'] = '+body[script|style]';
-        // allow empty elements
-        $settings['verify_html'] = false;
-        $settings['valid_elements'] = '*';
-        // stop wrapping everything in <p> tags (including <scripts>). Fixes error if <style> is first element in HTML editor.
-        // $settings['forced_root_block'] ='';
-
-        $result->setData('settings', $settings);
-        return $result;
     }
 }
