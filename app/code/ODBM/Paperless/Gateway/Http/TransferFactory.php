dfa194c8f6d838437d3d6abd02e9695c495c08b7
Peter Parker <peter.postma@odb.org>
2019-03-08 14:48:22 -0500

    use the config value instead of a hardcoded one

--- app/code/ODBM/Paperless/Gateway/Http/TransferFactory.php
+++ app/code/ODBM/Paperless/Gateway/Http/TransferFactory.php
@@ -14,13 +14,17 @@ class TransferFactory implements TransferFactoryInterface
 	 * @var TransferBuilder
 	 */
 	private $transferBuilder;
+	private $_config;
+
 	/**
 	 * @param TransferBuilder $transferBuilder
 	 */
 	public function __construct(
-		TransferBuilder $transferBuilder
+		TransferBuilder $transferBuilder,
+		\Magento\Framework\App\Config\ScopeConfigInterface $config
 	) {
 		$this->transferBuilder = $transferBuilder;
+		$this->_config = $config;
 	}
 	/**
 	 * Builds gateway transfer object
@@ -33,7 +37,14 @@ class TransferFactory implements TransferFactoryInterface
 		$request_details = $request['req'];
 		unset($request['req']);
 
-		$domain = /*from configs*/ 'https://api.paperlesstrans.com';
+		//merchant_gateway_key
+		//payment_domain
+		$config_value = $this->_config->getValue('payment/odbm_paperless/payment_domain', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+		if(!empty($config_value))
+			$domain = $config_value;
+		else
+			$domain = 'https://api.paperlesstrans.com';
+		
 		$url = $domain . $request_details['uri'];
 
 		$headrs = [
