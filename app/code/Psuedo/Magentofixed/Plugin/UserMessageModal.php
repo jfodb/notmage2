7c1b4d9e6bc5f2c88006ea81b722ce62a1ab3590
Peter Parker <peter.postma@odb.org>
2019-04-29 17:44:15 -0400

    use the same session tool as payment gateways

--- app/code/Psuedo/Magentofixed/Plugin/UserMessageModal.php
+++ app/code/Psuedo/Magentofixed/Plugin/UserMessageModal.php
@@ -13,7 +13,7 @@ namespace Psuedo\Magentofixed\Plugin;
 class UserMessageModal
 {
 	protected $session, $messages, $logger;
-	public function __construct(\Magento\Framework\Session\SessionManager $sessionManager, \Magento\Framework\Message\Manager $messages, \Psr\Log\LoggerInterface $log)
+	public function __construct(\Magento\Customer\Model\Session $sessionManager, \Magento\Framework\Message\Manager $messages, \Psr\Log\LoggerInterface $log)
 	{
 		$this->session = $sessionManager;
 		$this->messages = $messages;
@@ -25,11 +25,13 @@ class UserMessageModal
 
 		if(strpos($_SERVER['REQUEST_URI'], 'customer/section/load') && isset($_REQUEST['sections']) && strpos($_REQUEST['sections'], 'messages') !== false ){
 
+			//messages from gateway errors
 			if($msg1 = $this->session->getGatewayMessage()) {
 				$this->messages->addError($msg1, 'default');
 				$this->logger->notice("pulled session note for messages ".$msg1);
 				$this->session->unsGatewayMessage();
 			}
+			//general messages from other modules
 			if($msgs = $this->session->getUserMessages()){
 				foreach ($msgs as $msg) {
 					$this->messages->addError($msg, 'default');
@@ -37,7 +39,7 @@ class UserMessageModal
 				}
 				$this->session->unsUserMessages();
 			}
-		}
 
+		}
 	}
 }
