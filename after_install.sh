06f924dcdddf8666330a95492b09d2dcd2f3da38
Mike Dubinsky <mike.dubinsky@odb.org>
2019-05-07 14:02:17 -0400

    Back to original approach that should have worked to check logging

--- after_install.sh
+++ after_install.sh
@@ -31,8 +31,10 @@ aws s3 cp s3://wp.shared-files/"$DEPLOYMENT_GROUP_NAME"/cloudwatch/awslogs.conf 
 
 # TODO: After 2.3.1 update, remove the m2-hotfixes directory from GitHub and appspec.yml. Delete the below install command
 # Install the PRODSECBUG-2198 security patch (https://magento.com/security/patches/magento-2.3.1-2.2.8-and-2.1.17-security-update)
-git apply /usr/share/nginx/html/magento/m2-hotfixes/PRODSECBUG-2198-composer.patch
-# rm -rf /usr/share/nginx/html/magento/m2-hotfixes
+cd /usr/share/nginx/html/magento/
+git apply m2-hotfixes/PRODSECBUG-2198-composer.patch
+# rm -rf m2-hotfixes
+cd ~
 
 #build and deploy
 php /usr/share/nginx/html/magento/bin/magento setup:upgrade
