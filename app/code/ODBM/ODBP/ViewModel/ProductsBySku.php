5952a0cc480b4598ead674e5f7dc28f71f5e592a
mdubinsky <mike.dubinsky@odb.org>
2020-03-18 01:15:13 -0400

    Reorder layout and add filters to productrepository query

--- app/code/ODBM/ODBP/ViewModel/ProductsBySku.php
+++ app/code/ODBM/ODBP/ViewModel/ProductsBySku.php
@@ -7,39 +7,62 @@
 namespace ODBM\ODBP\ViewModel;
 
 use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Framework\Exception\NoSuchEntityException;
-use Psr\Log\LoggerInterface;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\Search\FilterGroupBuilder;
+use Magento\Framework\Api\SearchCriteriaBuilder;
 
 class ProductsBySku implements \Magento\Framework\View\Element\Block\ArgumentInterface
 {
     protected $productRepository;
+    protected $searchCriteriaBuilder;
+    protected $filterBuilder;
+    protected $filterGroupBuilder;
     protected $product;
-    protected $_logger;
-    protected $productArray = [];
+    protected $productFormats = [];
 
     public function __construct(
         ProductRepositoryInterface $productRepository,
-        LoggerInterface $logger
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        FilterBuilder $filterBuilder,
+        FilterGroupBuilder $filterGroupBuilder
     ) {
         $this->productRepository = $productRepository;
-        $this->_logger = $logger;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->filterBuilder = $filterBuilder;
+        $this->filterGroupBuilder = $filterGroupBuilder;
     }
 
     public function getProductsBySku($_productFormatSkus)
     {
-        $this->_logger->debug('Enter getProductsBySku: ');
 
+        // TODO: "AND" isn't working
+        // TODO: Showing disabled products
+
+        $filters = [];
+        $filter_group = [];
+
+        // build basic search for enabled products
+        $this->searchCriteriaBuilder->addFilter('status', Status::STATUS_ENABLED, 'eq');
+
+        // add "or" statements for each SKU
         foreach ($_productFormatSkus as $_sku) {
-            try{
-                $product = $this->productRepository->get($_sku);
-                $this->_logger->debug($product);
-            } catch (NoSuchEntityException $e) {
-                // do nothing and continue
-            }
-            if($product){
-                $productArray[] = $product;
-            }
+            $filters[] = $this->filterBuilder->setField('sku')
+                ->setValue($_sku)
+                ->setConditionType('eq')
+                ->create();
         }
-        return $productArray;
+
+        $filter_group[] = $this->filterGroupBuilder->setFilters($filters)->create();
+        $searchCriteria = $this->searchCriteriaBuilder->create()->setFilterGroups($filter_group);
+
+        $searchResults = $this->productRepository->getList($searchCriteria);
+        $productFormats = $searchResults->getItems();
+
+        if (isset($productFormats) && is_array($productFormats)) {
+            return $productFormats;
+        } else {
+            return [];
+        }
     }
 }
