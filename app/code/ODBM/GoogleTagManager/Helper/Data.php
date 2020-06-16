e016a3b18696ee9796c41cf84c40169796ae0284
mdubinsky <mike.dubinsky@odb.org>
2020-06-16 12:39:21 -0400

    Add Dynamic Google Tag Manager module

---
+++ app/code/ODBM/GoogleTagManager/Helper/Data.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * User: mdubinsky
+ * Date: 6/16/20
+ */
+
+namespace ODBM\GoogleTagManager\Helper;
+
+use  Magento\Framework\App\Helper\Context;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    const XML_PATH_ACTIVE = 'google/googletagmanager/active';
+    const XML_PATH_ACCOUNT = 'google/googletagmanager/containerid';
+
+    protected $_gtmActive;
+    protected $_accountId;
+
+    public function __construct(
+        Context $context
+    ) {
+        parent::__construct($context);
+        $this->_gtmActive = $this->scopeConfig->isSetFlag(self::XML_PATH_ACTIVE, ScopeInterface::SCOPE_STORE);
+        $this->_accountId = $this->scopeConfig->getValue(self::XML_PATH_ACCOUNT, ScopeInterface::SCOPE_STORE);
+    }
+
+    public function isEnabled()
+    {
+        return $this->_gtmActive && $this->_accountId;
+    }
+
+    public function getGtmCode()
+    {
+        return trim($this->_accountId);
+    }
+}
