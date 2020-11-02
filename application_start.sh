6891d901eb3526cb75c60d990acd7f9b600f34ed
BJ Clark <bjclark13@outlook.com>
2020-11-02 14:46:24 -0500

    Chown, don't chmod

--- application_start.sh
+++ application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MAGENTO=/usr/share/nginx/html/magento
 
-chmod -R 775 $MAGENTO/var $MAGENTO/pub $MAGENTO/app/etc
+# chmod -R 775 $MAGENTO/var $MAGENTO/pub $MAGENTO/app/etc
 chown -R apache:nginx $MAGENTO/*
 
 nginx -t
