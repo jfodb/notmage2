60c66fc4231606b936fdea8a26b362a5242f50ad
BJ Clark <bjclark13@outlook.com>
2020-11-02 11:25:37 -0500

    Re-build deployment scripts from scratch

--- after_install.sh
+++ after_install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-exec 2> /tmp/after_install.log
 
-if [[ $(findmnt -m /usr/share/nginx/html/magento/pub/media) ]]; then
+$MAGENTO=/usr/share/nginx/html/magento
+if [[ $(findmnt -m $MAGENTO/pub/media) ]]; then
     echo "Mounted"
 else
     if [ "$DEPLOYMENT_GROUP_NAME" == "donations-production" ]
     then
-        mount -t efs fs-1e74a656:/ /usr/share/nginx/html/magento/pub/media/
+        mount -t efs fs-1e74a656:/ $MAGENTO/pub/media/
     else
-        mount -t efs fs-e12571ab:/ /usr/share/nginx/html/magento/pub/media/
+        mount -t efs fs-e12571ab:/ $MAGENTO/pub/media/
     fi
 fi
 
@@ -16,24 +16,22 @@ fi
 usermod -a -G nginx,apache ec2-user
 
 # Pull from S3 based on deployment group
-aws s3 cp s3://wp.shared-files/"$DEPLOYMENT_GROUP_NAME"/env.php /usr/share/nginx/html/magento/app/etc/env.php
+aws s3 cp s3://wp.shared-files/"$DEPLOYMENT_GROUP_NAME"/env.php $MAGENTO/app/etc/env.php
 aws s3 cp s3://wp.shared-files/"$DEPLOYMENT_GROUP_NAME"/virtual.conf /etc/nginx/conf.d/virtual.conf
 aws s3 cp s3://wp.shared-files/"$DEPLOYMENT_GROUP_NAME"/cloudwatch/awslogs.conf /etc/awslogs/awslogs.confl
 
 #build and deploy
-php /usr/share/nginx/html/magento/bin/magento setup:upgrade
-php /usr/share/nginx/html/magento/bin/magento setup:di:compile
-php /usr/share/nginx/html/magento/bin/magento deploy:mode:set production
-php /usr/share/nginx/html/magento/bin/magento setup:static-content:deploy en_US es_MX
-php /usr/share/nginx/html/magento/bin/magento index:reindex
-php /usr/share/nginx/html/magento/bin/magento cron:install
+php $MAGENTO/bin/magento setup:upgrade
+php $MAGENTO/bin/magento setup:di:compile
+php $MAGENTO/bin/magento deploy:mode:set production
+php $MAGENTO/bin/magento setup:static-content:deploy en_US es_MX
+php $MAGENTO/bin/magento index:reindex
+php $MAGENTO/bin/magento cron:install
 
 #building makes bad cache
-php /usr/share/nginx/html/magento/bin/magento cache:clean
+php $MAGENTO/bin/magento cache:clean
 
-chown -R apache:nginx /usr/share/nginx/html/magento
+chmod -R 775 $MAGENTO/var $MAGENTO/pub
+chown -R apache:nginx $MAGENTO/*
 
 nginx -t
-
-# reload file changes and flushed logs
-service nginx restart
