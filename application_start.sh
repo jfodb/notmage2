5a141a53bde6faafe581d003ad17e98f60340294
BJ Clark <bjclark13@outlook.com>
2020-11-03 10:20:07 -0500

    Fix sed command

--- application_start.sh
+++ application_start.sh
@@ -2,7 +2,8 @@
 MAGENTO=/usr/share/nginx/html/magento
 
 # Set group as nginx for file creation in php-fpm
-sed -i s'/group = apache/group = nginx' /etc/php-fpm.d/www.conf
+sed -i s'/group = apache/group = nginx/' /etc/php-fpm.d/www.conf
+service php-fpm restart
 
 # chmod -R 775 $MAGENTO/var $MAGENTO/pub $MAGENTO/app/etc
 chown -R apache:nginx $MAGENTO/*
