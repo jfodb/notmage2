f1fee935dc67bb62888479828af6acd33df7dee1
BJ Clark <bjclark13@outlook.com>
2020-11-03 09:34:33 -0500

    Set group as nginx for file creation in php-fpm

--- application_start.sh
+++ application_start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 MAGENTO=/usr/share/nginx/html/magento
 
+# Set group as nginx for file creation in php-fpm
+sed -i s'/group = apache/group = nginx' /etc/php-fpm.d/www.conf
+
 # chmod -R 775 $MAGENTO/var $MAGENTO/pub $MAGENTO/app/etc
 chown -R apache:nginx $MAGENTO/*
 
