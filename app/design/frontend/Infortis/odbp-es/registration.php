465cef7cc02fe41c7bfb34a4cf77cf4df76ffd53
mdubinsky <mike.dubinsky@odb.org>
2020-12-03 10:17:56 -0500

    Change significant theme files for es store view

--- app/design/frontend/Infortis/odbp-es/registration.php
+++ app/design/frontend/Infortis/odbp-es/registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Infortis/odbp',
+    'frontend/Infortis/odbp-es',
     __DIR__
 );
