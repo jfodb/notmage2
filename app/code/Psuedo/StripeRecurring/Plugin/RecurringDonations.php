fc7369bfa306c0001b76f12b220a91ddc2bac9bf
Dennis Towns <dennis.towns@odb.org>
2020-12-08 16:03:18 -0500

    uslf-1689-recurring-module added plugin into recurring module

---
+++ app/code/Psuedo/StripeRecurring/Plugin/RecurringDonations.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * User: dtowns
+ * Date: 12/8/20
+ */
+
+namespace Psuedo\StripeRecurring\Plugin;
+use StripeIntegration\Payments\Helper\SetupIntent as SetupIntent;
+
+
+class RecurringDonations
+{
+
+    public function afterShouldUseSetupIntents(SetupIntent $subject, $result)
+    {
+        if ($this->helper->isAdmin())
+            return false;
+
+        if ($this->helper->hasSubscriptions())
+            return true;
+
+        return false;
+    }
+
+
+}
