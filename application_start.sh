12e3cbfa3200734c32751f36a14553efe7b1fb93
mdubinsky <mike.dubinsky@odb.org>
2021-06-23 10:35:39 -0400

    Add back in FIND var permissions commands to application_start.sh

--- application_start.sh
+++ application_start.sh
@@ -5,15 +5,14 @@ MAGENTO=/usr/share/nginx/html/magento
 sed -i s'/group = apache/group = nginx/' /etc/php-fpm.d/www.conf
 service php-fpm restart
 
-# chmod -R 775 $MAGENTO/var $MAGENTO/pub $MAGENTO/app/etc
+# Moved to after_install.sh because it seemed to be blocking deployments: 'Script at specified location: /application_start.sh failed to complete in 3600 seconds'
 # chown -R apache:nginx $MAGENTO/*
 
-# Removed because it was blocking deployments: 'Script at specified location: /application_start.sh failed to complete in 3600 seconds'
 # Permissions fix for Admin Export tool
 # For future permissions changes, consider this resource first:
 # https://docs.google.com/document/d/1bqAS6VZtT2uxWYbz5bqJW5s3zBrU2k7rcExqGdeRDqw/edit?usp=sharing
-# find $MAGENTO/var -type f -exec chmod g+w {} +
-# find $MAGENTO/var -type d -exec chmod g+ws {} +
+find $MAGENTO/var -type f -exec chmod g+w {} +
+find $MAGENTO/var -type d -exec chmod g+ws {} +
 
 nginx -t
 
