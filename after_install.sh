b233ddd8ba0dae481d3a4717d3ba519f56f29bda
Peter Parker <peter.postma@odb.org>
2021-08-24 15:05:27 -0400

    change how log files are configured.

--- after_install.sh
+++ after_install.sh
@@ -7,7 +7,7 @@ usermod -a -G nginx,apache ec2-user
 # Pull from S3 based on deployment group
 aws s3 cp s3://wp.shared-files/"$DEPLOYMENT_GROUP_NAME"/env.php $MAGENTO/app/etc/env.php
 aws s3 cp s3://wp.shared-files/"$DEPLOYMENT_GROUP_NAME"/virtual.conf /etc/nginx/conf.d/virtual.conf
-aws s3 cp s3://wp.shared-files/"$DEPLOYMENT_GROUP_NAME"/cloudwatch/awslogs.conf /etc/awslogs/awslogs.confl
+aws s3 cp s3://wp.shared-files/"$DEPLOYMENT_GROUP_NAME"/cloudwatch/ssm_beta-donations /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.d
 
 #build and deploy
 php $MAGENTO/bin/magento setup:upgrade
@@ -33,3 +33,6 @@ else
         mount -t efs fs-e12571ab:/ $MAGENTO/pub/media/
     fi
 fi
+
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a stop
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a start
