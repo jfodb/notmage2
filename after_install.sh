c90e093c49ab97a40ab25f90010ecb785fada490
Joe Fahnestock <joe.fahnestock@odb.org>
2021-11-12 13:56:32 -0500

    Move media symlink

--- after_install.sh
+++ after_install.sh
@@ -15,6 +15,25 @@ perl -pi -e s/$(echo odb_db_password)/$(aws secretsmanager get-secret-value --re
 # set db user
 perl -pi -e s/$(echo odb_db_user)/$(aws secretsmanager get-secret-value --region us-east-1 --secret-id $DEPLOYMENT_GROUP_NAME-credentials | jq -r '.SecretString' | jq -r '.username')/g $MAGENTO/app/etc/env.php
 
+# Check to see if we need
+# to re-create symlink to EFS
+# mount
+media_link=$MAGENTO/pub/media
+if [ -L ${media_link} ] ; then
+    if [ -e ${media_link} ] ; then
+        echo "Symlink already exists"
+    else
+        echo "Broken symlink for media folder"
+    fi
+    elif [ -e ${media_link} ] ; then
+    echo "Not a link, removing existing media"
+    rm -rf $MAGENTO/pub/media
+    ln -s /mnt/efs/fs1/media $MAGENTO/pub/
+else
+    echo "Creating symlink"
+    ln -s /mnt/efs/fs1/media $MAGENTO/pub/
+fi
+
 ## add redis configuration for cache
 $MAGENTO/bin/magento setup:config:set --cache-backend=redis --cache-backend-redis-server=$(aws ssm get-parameter --region us-east-1 --name "$DEPLOYMENT_GROUP_NAME-redis-endpoint" | jq -r ".Parameter.Value") --cache-backend-redis-db=0 -n
 
@@ -43,24 +62,6 @@ chown -R nginx:nginx $MAGENTO/*
 # TODO: test removing media from this list
 cd $MAGENTO && find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} + && find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} + && chown -R nginx:nginx . && chmod u+x bin/magento
 
-# Check to see if we need
-# to re-create symlink to EFS
-# mount
-media_link=$MAGENTO/pub/media
-if [ -L ${media_link} ] ; then
-    if [ -e ${media_link} ] ; then
-        echo "Symlink already exists"
-    else
-        echo "Broken symlink for media folder"
-    fi
-    elif [ -e ${media_link} ] ; then
-    echo "Not a link, removing existing media"
-    rm -rf $MAGENTO/pub/media
-    ln -s /mnt/efs/fs1/media $MAGENTO/pub/
-else
-    echo "Creating symlink"
-    ln -s /mnt/efs/fs1/media $MAGENTO/pub/
-fi
 
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a stop
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a start
